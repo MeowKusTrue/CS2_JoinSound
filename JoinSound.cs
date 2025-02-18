8f9cbcf24eeb4a3624678e84c0a8767e20c98713
Meowkus <dramdubas@gmail.com>
2025-02-19 00:40:29 +0300

    CS2_JoinSound

---
+++ JoinSound.cs
@@ -0,0 +1,68 @@
+﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core.Attributes.Registration;
+using CounterStrikeSharp.API.Modules.Commands;
+using CounterStrikeSharp.API.Modules.Entities;
+using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+using System.Text.Json;
+
+namespace CS2_JoinSound
+{
+    public class JoinSound : BasePlugin, IPluginConfig<PluginConfig>
+    {
+        public override string ModuleName => "JoinSound";
+        public override string ModuleVersion => "0.0.1";
+        public override string ModuleAuthor => "MeowKus";
+
+        public PluginConfig Config { get; set; } = new PluginConfig();
+
+        public void OnConfigParsed(PluginConfig config)
+        {
+            Config = config;
+            Console.WriteLine("Configuration loaded successfully.");
+        }
+
+        public override void Load(bool hotReload)
+        {
+            base.Load(hotReload);
+
+            RegisterListener<Listeners.OnClientPutInServer>(OnClientPutInServer);
+        }
+
+        private void OnClientPutInServer(int playerSlot)
+        {
+            var player = Utilities.GetPlayerFromSlot(playerSlot);
+            if (player != null)
+            {
+                PlayJoinSound(player);
+            }
+            else
+            {
+                Console.WriteLine($"Player with slot {playerSlot} not found.");
+            }
+        }
+
+        private void PlayJoinSound(CCSPlayerController player)
+        {
+            try
+            {
+                string musicFile = Config?.MusicFile ?? "default_sound_path.vsnd";
+                player.ExecuteClientCommand($"play {musicFile}");
+
+                Console.WriteLine($"Playing music for player");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error while playing music: {ex.Message}");
+            }
+        }
+
+        [ConsoleCommand("play_music")]
+        public void PlayMusicCommand(CCSPlayerController controller, CommandInfo info)
+        {
+            PlayJoinSound(controller);
+        }
+    }
+}
---
+++ PluginConfig.cs
@@ -0,0 +1,11 @@
+﻿using CounterStrikeSharp.API.Core;
+using System.Text.Json.Serialization;
+
+namespace CS2_JoinSound
+{
+    public class PluginConfig : BasePluginConfig
+    {
+        [JsonPropertyName("path")]
+        public string MusicFile { get; set; } = "dpmusic3/d1.vsnd";
+    }
+}
